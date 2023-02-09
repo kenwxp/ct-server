ec0240b59201f8283659f1126237b1ea95129fbc
TANK <it_luo@126.com>
2023-02-09 16:46:53 +0800

    数据字典

---
+++ cloudtimes-business/src/main/java/com/cloudtimes/app/controller/system/SysDictDataController.java
@@ -0,0 +1,66 @@
+package com.cloudtimes.app.controller.system;
+
+import com.cloudtimes.common.core.controller.BaseController;
+import com.cloudtimes.common.core.domain.AjaxResult;
+import com.cloudtimes.common.core.domain.entity.SysDictData;
+import com.cloudtimes.common.core.page.TableDataInfo;
+import com.cloudtimes.common.utils.StringUtils;
+import com.cloudtimes.system.service.ISysDictDataService;
+import com.cloudtimes.system.service.ISysDictTypeService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 数据字典信息
+ *
+ * @author tank
+ */
+@Api(tags = "数据字典信息")
+@RestController
+@RequestMapping("/system/dict/data")
+public class SysDictDataController extends BaseController {
+    @Autowired
+    private ISysDictDataService dictDataService;
+
+    @Autowired
+    private ISysDictTypeService dictTypeService;
+
+    @ApiOperation("查询数据字典信息列表")
+    @GetMapping("/list")
+    public TableDataInfo list(SysDictData dictData) {
+        startPage();
+        List<SysDictData> list = dictDataService.selectDictDataList(dictData);
+        return getDataTable(list);
+    }
+
+    /**
+     * 查询字典数据详细
+     */
+    @ApiOperation("查询字典数据详细")
+    @GetMapping(value = "/{dictCode}")
+    public AjaxResult getInfo(@PathVariable Long dictCode) {
+        return AjaxResult.success(dictDataService.selectDictDataById(dictCode));
+    }
+
+    /**
+     * 根据字典类型查询字典数据信息
+     */
+    @ApiOperation("根据字典类型查询字典数据信息")
+    @GetMapping(value = "/type/{dictType}")
+    public AjaxResult dictType(@PathVariable String dictType) {
+        List<SysDictData> data = dictTypeService.selectDictDataByType(dictType);
+        if (StringUtils.isNull(data)) {
+            data = new ArrayList<SysDictData>();
+        }
+        return AjaxResult.success(data);
+    }
+
+}
