e125cbf79e97fbc9d667310afac8b3a37fc96d3a
kenwxp <kenwxp007@163.com>
2023-02-10 11:05:05 +0800

    调整mq处理流程模式

--- cloudtimes-socketserver/src/main/java/com/cloudtimes/app/mq/ListenOpenDoor.java
+++ cloudtimes-socketserver/src/main/java/com/cloudtimes/app/mq/ListenOpenDoor.java
@@ -1,8 +1,8 @@
-package com.cloudtimes.mq.listen;
+package com.cloudtimes.app.mq;
 
 import com.alibaba.fastjson.JSONObject;
 import com.cloudtimes.common.constant.RocketMQConstants;
-import com.cloudtimes.mq.models.MessageBody;
+import com.cloudtimes.common.mq.MessageBody;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.spring.annotation.MessageModel;
 import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
@@ -16,7 +16,7 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 @Component
-@RocketMQMessageListener(consumerGroup = "${rocketmq.consumer.group}", topic = RocketMQConstants.TOPIC_DEVICE, messageModel = MessageModel.BROADCASTING)
+@RocketMQMessageListener(consumerGroup = "${rocketmq.consumer.group}", topic = RocketMQConstants.WS_TOPIC_DEVICE, messageModel = MessageModel.CLUSTERING)
 public class ListenOpenDoor implements RocketMQListener<MessageBody> {
 
     @Override
