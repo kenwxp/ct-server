aa4ffb4a66b865aef6d4b357827a30da3b332606
kenwxp <kenwxp007@163.com>
2023-02-14 12:41:51 +0800

    优化http调用工具类

--- cloudtimes-common/src/main/java/com/cloudtimes/common/utils/http/HttpUtils.java
+++ cloudtimes-common/src/main/java/com/cloudtimes/common/utils/http/HttpUtils.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.net.*;
 import java.nio.charset.StandardCharsets;
 import java.security.cert.X509Certificate;
+import java.util.HashMap;
 import java.util.Map;
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
@@ -39,12 +40,26 @@ public class HttpUtils {
     /**
      * 向指定 URL 发送GET方法的请求
      *
-     * @param url   发送请求的 URL
-     * @param param 请求参数，请求参数应该是 name1=value1&name2=value2 的形式。
+     * @param url 发送请求的 URL
      * @return 所代表远程资源的响应结果
      */
     public static String sendGet(String url, String param) {
         return sendGet(url, param, Constants.UTF8);
+    }
+
+    /**
+     * 向指定 URL 发送GET方法的请求
+     *
+     * @param url   发送请求的 URL
+     * @param param 请求参数，请求参数应该是 name1=value1&name2=value2 的形式。
+     * @return 所代表远程资源的响应结果
+     */
+    public static String sendGet(String url, String param, String contentType) {
+        HashMap<String, String> header = new HashMap<>();
+        header.put("accept", "*/*");
+//        header.put("connection", "Keep-Alive");
+        header.put("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+        return sendGet(url, param, contentType, header);
     }
 
     /**
@@ -55,7 +70,7 @@ public class HttpUtils {
      * @param contentType 编码类型
      * @return 所代表远程资源的响应结果
      */
-    public static String sendGet(String url, String param, String contentType) {
+    public static String sendGet(String url, String param, String contentType, Map<String, String> header) {
         StringBuilder result = new StringBuilder();
         BufferedReader in = null;
         try {
@@ -63,9 +78,12 @@ public class HttpUtils {
             log.info("sendGet - {}", urlNameString);
             URL realUrl = new URL(urlNameString);
             URLConnection connection = realUrl.openConnection();
-            connection.setRequestProperty("accept", "*/*");
-            connection.setRequestProperty("connection", "Keep-Alive");
-            connection.setRequestProperty("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+            //设置请求头
+            if (header != null) {
+                for (Map.Entry<String, String> e : header.entrySet()) {
+                    connection.setRequestProperty(e.getKey(), e.getValue());
+                }
+            }
             connection.connect();
             in = new BufferedReader(new InputStreamReader(connection.getInputStream(), contentType));
             String line;
@@ -96,11 +114,91 @@ public class HttpUtils {
     /**
      * 向指定 URL 发送POST方法的请求
      *
+     * @param url    发送请求的 URL
+     * @param params 请求体参数，请求参数应该是 map key value形式。
+     * @return 所代表远程资源的响应结果
+     */
+    public static String sendJsonPost(String url, String params) throws Exception {
+        HashMap<String, String> header = new HashMap<>();
+        header.put("Content-Type", "application/json; charset=utf-8");
+        header.put("Accept", "application/json; charset=utf-8");
+        return sendJsonPost(url, params, header, 5000, 30000);
+    }
+
+    /**
+     * 向指定 URL 发送POST方法的请求
+     *
+     * @param url    发送请求的 URL
+     * @param params 请求体参数，请求参数应该是 map key value形式。
+     * @param header 请求头参数，请求参数应该是 map key value形式。
+     * @return 所代表远程资源的响应结果
+     */
+    public static String sendJsonPost(String url, String params, Map<String, String> header) throws Exception {
+        return sendJsonPost(url, params, header, 5000, 30000);
+    }
+
+    /**
+     * 向指定 URL 发送POST方法的请求
+     *
+     * @param url            发送请求的 URL
+     * @param params         请求体参数，请求参数应该是 map key value形式。
+     * @param header         请求头参数，请求参数应该是 map key value形式。
+     * @param readTimeout    读超时 单位毫秒
+     * @param connectTimeout 连接超时 单位毫秒
+     * @return 所代表远程资源的响应结果
+     */
+    public static String sendJsonPost(String url, String params, Map<String, String> header, int readTimeout, int connectTimeout) throws Exception {
+        return sendPost(url, params, header, readTimeout, connectTimeout);
+    }
+
+    /**
+     * 向指定 URL 发送POST方法的请求
+     *
+     * @param url    发送请求的 URL
+     * @param params 请求体参数，请求参数应该是 map key value形式。
+     * @param header 请求头参数，请求参数应该是 map key value形式。
+     * @return 所代表远程资源的响应结果
+     */
+    public static String sendFormPost(String url, Map<String, String> params, Map<String, String> header) {
+        StringBuffer sb = new StringBuffer();
+        if (params != null) {
+            for (Map.Entry<String, String> e : params.entrySet()) {
+                sb.append(e.getKey());
+                sb.append("=");
+                sb.append(e.getValue());
+                sb.append("&");
+            }
+            sb.substring(0, sb.length() - 1);
+        }
+        header.put("Content-Type", "application/x-www-form-urlencoded");
+        return sendPost(url, sb.toString(), header, 5000, 30000);
+    }
+
+    /**
+     * 向指定 URL 发送POST方法的请求
+     *
      * @param url   发送请求的 URL
      * @param param 请求参数，请求参数应该是 name1=value1&name2=value2 的形式。
      * @return 所代表远程资源的响应结果
      */
     public static String sendPost(String url, String param) {
+        HashMap<String, String> header = new HashMap<>();
+        header.put("accept", "*/*");
+//        header.put("connection", "Keep-Alive");
+        header.put("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+        header.put("Accept-Charset", "utf-8");
+        header.put("contentType", "utf-8");
+        return sendPost(url, param, header, 5000, 30000);
+    }
+
+    /**
+     * 向指定 URL 发送POST方法的请求
+     *
+     * @param url   发送请求的 URL
+     * @param param 请求参数，请求参数应该是 name1=value1&name2=value2 的形式。
+     * @return 所代表远程资源的响应结果
+     */
+    public static String sendPost(String url, String param, Map<String, String> header, int readTimeout, int connectTimeout) {
         PrintWriter out = null;
         BufferedReader in = null;
         StringBuilder result = new StringBuilder();
@@ -108,13 +206,16 @@ public class HttpUtils {
             log.info("sendPost - {}", url);
             URL realUrl = new URL(url);
             URLConnection conn = realUrl.openConnection();
-            conn.setRequestProperty("accept", "*/*");
-            conn.setRequestProperty("connection", "Keep-Alive");
-            conn.setRequestProperty("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
-            conn.setRequestProperty("Accept-Charset", "utf-8");
-            conn.setRequestProperty("contentType", "utf-8");
+            //设置请求头
+            if (header != null) {
+                for (Map.Entry<String, String> e : header.entrySet()) {
+                    conn.setRequestProperty(e.getKey(), e.getValue());
+                }
+            }
             conn.setDoOutput(true);
             conn.setDoInput(true);
+            conn.setConnectTimeout(connectTimeout);
+            conn.setReadTimeout(readTimeout);
             out = new PrintWriter(conn.getOutputStream());
             out.print(param);
             out.flush();
@@ -163,7 +264,6 @@ public class HttpUtils {
             conn.setRequestProperty("contentType", "utf-8");
             conn.setDoOutput(true);
             conn.setDoInput(true);
-
             conn.setSSLSocketFactory(sc.getSocketFactory());
             conn.setHostnameVerifier(new TrustAnyHostnameVerifier());
             conn.connect();
@@ -186,167 +286,6 @@ public class HttpUtils {
             log.error("调用HttpUtils.sendSSLPost IOException, url=" + url + ",param=" + param, e);
         } catch (Exception e) {
             log.error("调用HttpsUtil.sendSSLPost Exception, url=" + url + ",param=" + param, e);
-        }
-        return result.toString();
-    }
-
-    /**
-     * 向指定 URL 发送POST方法的请求
-     *
-     * @param url    发送请求的 URL
-     * @param params 请求体参数，请求参数应该是 map key value形式。
-     * @param header 请求头参数，请求参数应该是 map key value形式。
-     * @return 所代表远程资源的响应结果
-     */
-    public static String sendFormPost(String url, Map<String, String> params, Map<String, String> header) {
-        PrintWriter out = null;
-        BufferedReader in = null;
-        StringBuilder result = new StringBuilder();
-        HttpURLConnection conn = null;
-        try {
-            // 构建Form请求参数
-            StringBuffer sb = new StringBuffer();
-            if (params != null) {
-                for (Map.Entry<String, String> e : params.entrySet()) {
-                    sb.append(e.getKey());
-                    sb.append("=");
-                    sb.append(e.getValue());
-                    sb.append("&");
-                }
-                sb.substring(0, sb.length() - 1);
-            }
-            System.out.println("send_url:" + url);
-            System.out.println("send_data:" + sb.toString());
-            URL realUrl = new URL(url);
-            conn = (HttpURLConnection) realUrl.openConnection();
-            conn.setRequestMethod("POST");
-            conn.setDoOutput(true);
-            conn.setDoInput(true);
-            conn.setUseCaches(false);
-            conn.setConnectTimeout(30000);
-            conn.setReadTimeout(5000);
-            //设置请求头
-            if (header != null) {
-                for (Map.Entry<String, String> e : header.entrySet()) {
-                    conn.setRequestProperty(e.getKey(), e.getValue());
-                }
-            }
-            OutputStreamWriter osw = new OutputStreamWriter(conn.getOutputStream(), StandardCharsets.UTF_8);
-            osw.write(sb.toString());
-            osw.flush();
-            osw.close();
-            in = new BufferedReader(new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8));
-            String line;
-            while ((line = in.readLine()) != null) {
-                result.append(line);
-            }
-            log.info("recv - {}", result);
-        } catch (ConnectException e) {
-            log.error("调用HttpUtils.sendPost ConnectException, url=" + url + ",param=" + params, e);
-        } catch (SocketTimeoutException e) {
-            log.error("调用HttpUtils.sendPost SocketTimeoutException, url=" + url + ",param=" + params, e);
-        } catch (IOException e) {
-            log.error("调用HttpUtils.sendPost IOException, url=" + url + ",param=" + params, e);
-        } catch (Exception e) {
-            log.error("调用HttpsUtil.sendPost Exception, url=" + url + ",param=" + params, e);
-        } finally {
-            try {
-                if (conn != null) {
-                    conn.disconnect();
-                }
-                if (out != null) {
-                    out.close();
-                }
-                if (in != null) {
-                    in.close();
-                }
-            } catch (IOException ex) {
-                log.error("调用in.close Exception, url=" + url + ",param=" + params, ex);
-            }
-        }
-        return result.toString();
-    }
-
-    /**
-     * 向指定 URL 发送POST方法的请求
-     *
-     * @param url    发送请求的 URL
-     * @param params 请求体参数，请求参数应该是 map key value形式。
-     * @param header 请求头参数，请求参数应该是 map key value形式。
-     * @return 所代表远程资源的响应结果
-     */
-    public static String sendJsonPost(String url, String params, Map<String, String> header) throws Exception {
-        return sendJsonPost(url, params, header, 5000, 30000);
-    }
-
-    /**
-     * 向指定 URL 发送POST方法的请求
-     *
-     * @param url            发送请求的 URL
-     * @param params         请求体参数，请求参数应该是 map key value形式。
-     * @param header         请求头参数，请求参数应该是 map key value形式。
-     * @param readTimeout    读超时 单位毫秒
-     * @param connectTimeout 连接超时 单位毫秒
-     * @return 所代表远程资源的响应结果
-     */
-    public static String sendJsonPost(String url, String params, Map<String, String> header, int readTimeout, int connectTimeout) throws Exception {
-        PrintWriter out = null;
-        BufferedReader in = null;
-        StringBuilder result = new StringBuilder();
-        HttpURLConnection conn = null;
-        try {
-            System.out.println("send_url:" + url);
-            System.out.println("send_data:" + params);
-            URL realUrl = new URL(url);
-            conn = (HttpURLConnection) realUrl.openConnection();
-            conn.setRequestMethod("POST");
-            conn.setDoOutput(true);
-            conn.setDoInput(true);
-            conn.setUseCaches(false);
-            conn.setConnectTimeout(connectTimeout);
-            conn.setReadTimeout(readTimeout);
-            //设置请求头
-            if (header != null) {
-                for (Map.Entry<String, String> e : header.entrySet()) {
-                    conn.setRequestProperty(e.getKey(), e.getValue());
-                }
-            }
-            OutputStreamWriter osw = new OutputStreamWriter(conn.getOutputStream(), StandardCharsets.UTF_8);
-            osw.write(params);
-            osw.flush();
-            osw.close();
-            in = new BufferedReader(new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8));
-            String line;
-            while ((line = in.readLine()) != null) {
-                result.append(line);
-            }
-            log.info("recv - {}", result);
-        } catch (ConnectException e) {
-            log.error("调用HttpUtils.sendPost ConnectException, url=" + url + ",param=" + params, e);
-            throw e;
-        } catch (SocketTimeoutException e) {
-            log.error("调用HttpUtils.sendPost SocketTimeoutException, url=" + url + ",param=" + params, e);
-            throw e;
-        } catch (IOException e) {
-            log.error("调用HttpUtils.sendPost IOException, url=" + url + ",param=" + params, e);
-            throw e;
-        } catch (Exception e) {
-            log.error("调用HttpsUtil.sendPost Exception, url=" + url + ",param=" + params, e);
-            throw e;
-        } finally {
-            try {
-                if (conn != null) {
-                    conn.disconnect();
-                }
-                if (out != null) {
-                    out.close();
-                }
-                if (in != null) {
-                    in.close();
-                }
-            } catch (IOException ex) {
-                log.error("调用in.close Exception, url=" + url + ",param=" + params, ex);
-            }
         }
         return result.toString();
     }
