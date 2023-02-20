82b2cd22cb61c98258ad90c619ccd1dcbc37840c
kenwxp <kenwxp007@163.com>
2023-02-20 14:23:32 +0800

    修复编译报错

--- cloudtimes-business/src/main/java/com/cloudtimes/app/controller/cash/model/CashLoginResp.java
+++ cloudtimes-business/src/main/java/com/cloudtimes/app/controller/cash/model/CashLoginResp.java
@@ -4,10 +4,23 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+
 @Data
 @Slf4j
 @ApiModel(value = "CashLoginResp", description = "小程序登录校验接口返回体")
 public class CashLoginResp {
     @ApiModelProperty("后台登录token")
     private String accessToken;
+    @ApiModelProperty("是否值守 0-否 1-是")
+    private String isSupervise;
+    @ApiModelProperty("动态码")
+    private String dynamicQrCode;
+    @ApiModelProperty("门店号")
+    private String shopNo;       // 门店号
+    @ApiModelProperty("门店名")
+    private String shopName;     // 门店名
+    @ApiModelProperty("联系人姓名")
+    private String contactName;  // 联系人姓名
+    @ApiModelProperty("联系人手机")
+    private String contactPhone; // 联系人手机
 }
