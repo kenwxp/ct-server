b51d0391eabe3f62ca56bebaca05c78e550e2a5f
TANK <it_luo@126.com>
2023-02-27 22:07:14 +0800

    111

--- cloudtimes-detectionserver/src/main/java/com/cloudtimes/DetectionServerApplication.java
+++ cloudtimes-detectionserver/src/main/java/com/cloudtimes/DetectionServerApplication.java
@@ -2,26 +2,20 @@ package com.cloudtimes;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
  * 启动程序
  *
  * @author tank
  */
-@EnableWebMvc
 @MapperScan("com.cloudtimes.**.mapper")
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class DetectionServerApplication {
     public static void main(String[] args) {
         // System.setProperty("spring.devtools.restart.enabled", "false");
-        SpringApplication application = new SpringApplication(DetectionServerApplication.class);
-        application.setWebApplicationType(WebApplicationType.NONE);
-        application.run(args);
+        SpringApplication.run(DetectionServerApplication.class, args);
         System.out.println("云时代检测服务启动成功");
     }
 }
